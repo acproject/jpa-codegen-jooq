cb16c4b8b07cc40136c0a4dfcf270844c1172a95
acproject <acproject@qq.com>
2025-02-23 17:03:52 +0800

    增加分页的工具类

--- src/main/java/com/owiseman/jpa/util/PaginationHelper.java
+++ src/main/java/com/owiseman/jpa/util/PaginationHelper.java
@@ -4,9 +4,12 @@ import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
 
+import java.util.List;
+
 public class PaginationHelper {
     /**
      * Select total all pages
+     *
      * @param dslContext
      * @param condition
      * @param tableName
@@ -14,7 +17,8 @@ public class PaginationHelper {
      * @return
      */
     public static int getTotalPages(DSLContext dslContext, Condition condition,
-                                      String tableName, int pageSize){
+                                    String tableName, int pageSize) {
+        Condition Con = DSL.noCondition();
         int totalRecords = dslContext
                 .selectCount()
                 .from(DSL.table(tableName))
@@ -22,4 +26,25 @@ public class PaginationHelper {
                 .fetchOne(0, int.class);
         return (int) Math.ceil((double) totalRecords / pageSize);
     }
+
+     @SuppressWarnings("unchecked")
+    public static <T> List<T> getPaginatedData(DSLContext dslContext,
+                                               Condition condition,  // 允许传入 null
+                                               String tableName,
+                                               int pageSize,
+                                               int pageNumber,
+                                               Class<T> recordClass) {
+        // 处理空条件
+        Condition finalCondition = condition != null ? condition : DSL.noCondition();
+
+        return dslContext
+                .select()
+                .from(DSL.table(tableName))
+                .where(finalCondition)
+                .limit(pageSize)
+                .offset((pageNumber - 1) * pageSize)
+                .fetchInto(recordClass);
+    }
+
+
 }
