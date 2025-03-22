7c9e1b1267a288fb943713216af09f64a8c29b3c
acproject <acproject@qq.com>
2025-03-22 08:22:25 +0800

    fix: 增加扩展DSL的工具类

--- src/main/java/com/owiseman/jpa/util/DSLUtil.java
+++ src/main/java/com/owiseman/jpa/util/DSLUtil.java
@@ -4,7 +4,7 @@ import org.jooq.Field;
 import org.jooq.Table;
 import org.jooq.impl.DSL;
 
-public class DSLUtil extends DSL {
+public class DSLUtil  {
     public static Field<?> getTableDotFieldName(Table<?> table, Field<?> field) {
         return DSL.field(table.getName() + "." + field.getName());
     }
